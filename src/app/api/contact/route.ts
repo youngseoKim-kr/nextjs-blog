496f07f68a880e9cc6430ab8d122fe4c5b44656d
youngseoKim-kr <ysk5754@naver.com>
2023-10-10 14:37:09 +0900

    email api

--- src/app/api/contact/route.ts
+++ src/app/api/contact/route.ts
@@ -8,9 +8,28 @@ const bodySchema = yup.object().shape({
 });
 
 export async function POST(req: Request) {
-  if (!bodySchema.isValidSync(req.body)) {
-    return new Response("유효하지 않는 포맷", { status: 400 });
+  const body = await req.json();
+  if (!bodySchema.isValidSync(body)) {
+    return new Response(JSON.stringify({ message: "메일 전송애 실패함!" }), {
+      status: 400,
+    });
   }
-  const { from, subject, message } = req.body;
+
   //nodemailer
+  return sendEmail(body)
+    .then(
+      () =>
+        new Response(
+          JSON.stringify({
+            message: "메일을 성공적으로 보냈음",
+          }),
+          { status: 200 }
+        )
+    )
+    .catch((error) => {
+      console.log(error);
+      return new Response(JSON.stringify({ message: "메일 전송애 실패함!" }), {
+        status: 500,
+      });
+    });
 }
